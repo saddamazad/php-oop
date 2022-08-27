1bfa59a438753872a986864543882a744fe54a6f
Saddam Hossain Azad <saddam987020@gmail.com>
2022-08-27 17:33:35 +0600

    Use of Constants inside Class

---
+++ constant-in-class.php
@@ -0,0 +1,26 @@
+<?php
+//define ("VER", 123);
+
+class MyClass {
+    /**
+     * Can't use `define` keyword to create constant inside a Class
+     * Constant is ALWAYS in `static` scope inside a Class, so can't be accessed like a public property
+     */
+    const CITY = "Dhaka";
+
+    //define ("CITY", "Dhaka");
+
+    public function sayHi() {
+        echo "Hi from ".self::CITY."<br>";
+    }
+}
+
+$m = new MyClass();
+$m->sayHi();
+
+
+echo "<br>";
+echo $m::CITY;
+
+echo "<br>";
+echo MyClass::CITY;
