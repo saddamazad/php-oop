c7bea0b1e526900bdcac7826f8897c0157d3bb3c
Saddam Hossain Azad <saddam987020@gmail.com>
2022-08-28 01:27:06 +0600

    Comparing Objects

---
+++ object-compare.php
@@ -0,0 +1,27 @@
+<?php
+class Planet {
+    public $name;
+
+    public function __construct($name) {
+        $this->name = $name;
+    }
+}
+
+$e = new Planet("Earth");
+$e1 = new Planet("Earth");
+$m = new Planet("Mars");
+
+// if($e == $m) {
+//     echo "Similar Planets";
+// } else {
+//     echo "Not Similar";
+// }
+
+//$e1->name = "Earth 2";
+
+/* always compare objects with type, `===` should be used instead of `==` */
+if($e === $e1) {
+    echo "Similar Planets";
+} else {
+    echo "Not Similar";
+}
