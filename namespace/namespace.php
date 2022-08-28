fac846a1b05990ac26a0a55fe9b703377bcd1376
Saddam Hossain Azad <saddam987020@gmail.com>
2022-08-29 00:42:26 +0600

    Namespace Basics

--- namespace/namespace.php
+++ namespace/namespace.php
@@ -4,10 +4,15 @@ namespace Astronomy;
 
 include "planet.php";
 
-$planet = new \Astronomy\Planet(); // Qualified Name
+$planet = new \Astronomy\Planet(); // Qualified Name, always begis with a `backslash`
 $planet->getName();
 
 echo "<br>";
+
+/**
+ * Namespace prefixes the Class name with `\namespace_name\` WHEN `Unqualified Name` is used to initialize an object
+ * In this file the name of the namespace is Astronomy, so it will prefix the Class with `\Astronomy\`
+ */
 $planet2 = new Planet(); /* This also works, but it's `Unqualified Name` */
 $planet2->getName();
 
