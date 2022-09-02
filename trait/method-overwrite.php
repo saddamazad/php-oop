f9fabef8c4cf55885c7d9f035ae9853268c59e99
Saddam Hossain Azad <saddam987020@gmail.com>
2022-09-03 04:07:53 +0600

    `Trait`'s method overwriting precedence/order/priority

---
+++ trait/method-overwrite.php
@@ -0,0 +1,49 @@
+<?php
+trait NumberSeriesOne {
+    public function numberSeriesA() {
+        echo "Number Series A<br>";
+
+        /**
+         * useful if any Class `extends` a Class that overwrites this function, then overwritten function from the `parent` Class will be executed instead of the trait
+         * 
+         */
+        parent::numberSeriesA();
+    }
+
+    public function numberSeriesB() {
+        echo "Number Series B<br>";
+    }
+}
+
+class NumberSeries {
+    // call/use the traits
+    use NumberSeriesOne;
+
+    public function numberSeriesA() {
+        echo "Printing Number Series A<br>";
+    }
+}
+
+$ns = new numberSeries();
+
+// it will call the function from the Class, NOT from the trait, because it's overwritten by the Class
+$ns->numberSeriesA();
+
+
+
+
+class SomeClass {
+    public function numberSeriesA() {
+        echo "Printing Number Series A<br>";
+    }
+}
+
+class SomeClassChild extends SomeClass {
+    // call/use the traits
+    use NumberSeriesOne;
+}
+
+$scc = new SomeClassChild();
+
+// it will call the function from the Trait, NOT from the Class/Parent Class, because when a Class `extends` another Class(`parent`) that overwrites a function from a Trait, the `child` Class will execute the function from the `Trait`
+$scc->numberSeriesA();
