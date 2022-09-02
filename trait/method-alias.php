53fdac30ae84d7f1f3a6b531b71206d9dafd1d93
Saddam Hossain Azad <saddam987020@gmail.com>
2022-09-03 04:29:29 +0600

    Trait method renaming/alias - Conflict resolution

---
+++ trait/method-alias.php
@@ -0,0 +1,36 @@
+<?php
+trait NumberSeriesOne {
+    public function numberSeriesA() {
+        echo "Number Series One A<br>";
+    }
+}
+
+trait NumberSeriesTwo {
+    public function numberSeriesA() {
+        echo "Number Series Two A<br>";
+    }
+}
+
+class NumberSeries {
+    // call/use the traits, and `alias` the methods name
+    /*use NumberSeriesOne {
+        numberSeriesA as nsA;
+    }*/
+
+
+    // call/use the traits, and `alias` the same name methods from multiple traits
+    use NumberSeriesOne, NumberSeriesTwo {
+        NumberSeriesOne::numberSeriesA as nsA;
+        NumberSeriesTwo::numberSeriesA as nsAA;
+    }
+
+    public function numberSeriesA() {
+        echo "Printing Number Series A<br>";
+    }
+}
+
+$ns = new numberSeries();
+
+$ns->nsA();
+echo "<br>";
+$ns->nsAA();
