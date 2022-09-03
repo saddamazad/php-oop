49188cfd87d09dd7e5022ea71523c0cf4d6e4b3c
Saddam Hossain Azad <saddam987020@gmail.com>
2022-09-03 21:02:50 +0600

    Concepts of Method Chaining

---
+++ advanced/method-chaining.php
@@ -0,0 +1,46 @@
+<?php
+class StringUtility {
+    private $string;
+    private $search;
+
+    public function __construct($string) {
+        $this->string = $string;
+    }
+
+    public function search($string) {
+        $this->search = $string;
+
+        // return the object itself, this is MUST for method chaining - all the methods should return the object itself
+        return $this;
+    }
+
+    public function replace($string) {
+        if( ! isset($this->search) ) {
+            throw new Exception("Nothing to replace");
+        } else {
+            $this->string = str_replace($this->search, $string, $this->string);
+            return $this;
+        }
+    }
+
+    public function upperCase() {
+        $this->string = strtoupper($this->string);
+        return $this;
+    }
+
+    public function lowerCase() {
+        $this->string = strtolower($this->string);
+
+        // return the object itself, this is MUST for method chaining - all the methods should return the object itself
+        return $this;
+    }
+
+    public function print() {
+        echo $this->string;
+    }
+}
+
+$su = new StringUtility("Hello World");
+//$su->search("Hello")->replace("Hi")->upperCase()->lowerCase()->print();
+
+$su->search("Hello")->replace("Hi")->search("World")->replace("Earth")->upperCase()->print();
